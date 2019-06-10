1c3928ae66d90bcfc782dc0922295bceda48757b
SnappJoep <joep@snappcar.nl>
2019-06-10 21:57:24 +0200

    order usings

--- Source/Ninstance/Instance.cs
+++ Source/Ninstance/Instance.cs
@@ -1,10 +1,11 @@
 ﻿namespace Ninstance
 {
-    using NSubstitute;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
+
+    using NSubstitute;
 
     public static class Instance
     {
